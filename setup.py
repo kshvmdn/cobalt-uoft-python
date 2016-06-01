09a05e3fd9186344a598ca0b3c7070a6de780872
Kashav Madan <kshvmdn@gmail.com>
2016-05-31 22:57:01 -0400

    Update pypi metadata

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
-from setuptools.core import setup
+from setuptools import setup
 
 setup(
     name='cobaltuoft',
-    version='0.0.1',
+    version='0.0.2',
     description='A wrapper for the Cobalt API.',
-    long_description='A Python library for interfacing with Cobalt\
-        (http://cobalt.qas.im), a University of Toronto Open Data API.',
+    long_description='A Python library for interfacing with Cobalt (http://cobalt.qas.im), a University of Toronto Open Data API.',
+    url='https://github.com/kshvmdn/cobalt-uoft-python',
     author='Kashav Madan',
     author_email='kshvmdn@gmail.com',
-    url='https://github.com/kshvmdn/cobaltuoft-python',
-    packages=[
-        'cobaltuoft'
-    ],
-    package_data={'': ['LICENSE']},
-    package_dir={},
-    include_package_data=True,
-    install_requires=[
-    ],
     license='MIT',
-    zip_safe=False
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=['cobalt', 'uoft', 'university of toronto', 'api wrapper', 'open data'],
+    packages=['cobaltuoft', 'cobaltuoft.endpoints', 'cobaltuoft.helpers'],
+    package_data={
+        '': ['LICENSE']
+    },
+    package_dir={'cobaltuoft': 'cobaltuoft'},
+    install_requires=['requests', 'bs4'],
+    include_package_data=True
 )
